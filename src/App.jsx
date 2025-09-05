bd0a7f54f54ef6853fc85ddf3e5d52e0b80a5f6b
Amrita <amritakataria515@gmail.com>
2025-09-06 02:22:08 +0530

    Add vercel.json for dependency fix

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router basename="/MyPortfolio">
+    <Router>
       <HeroLayout>
         <Navbar />
         <SwipeDeck />
